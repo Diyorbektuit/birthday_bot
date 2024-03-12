ec6ba0ed9d4aafcddc712c2b8f0d73f3ff0e004e
Diyorbektuit <140270731+Diyorbektuit@users.noreply.github.com>
2024-03-12 14:02:31 +0500

    for deploy

--- core/settings.py
+++ core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3p2#_^qi_$8#m%xkj0shpi*3&f90!iob(ei3x(rd4aoum%dev3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
